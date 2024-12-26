ee1367483c1ce0b0fcb265e1078a77b501de84aa
Milos <milos.mandicc@gmail.com>
2024-12-26 14:27:49 +0100

    testiranje

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Application
+﻿
+using Domain;
+using Domain.Models;
+using Domain.Enums;
+
+namespace Application
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Testiram,posle cemo obrisati ovo
+            TimeSpan span = TimeSpan.FromSeconds(10);
+            TimeSpan dnevniP = new TimeSpan(8, 0, 0);
+            TimeSpan dnevniK = new TimeSpan(20, 0, 0);
+
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+            Console.WriteLine(span);
+            Device d1 = new Device(1,span);
+            Device d2 = new Device(2,span);
+            Device d3 = new Device(3,span);
+            Device d4 = new Device(4,span);
+            Regulator r = new Regulator(RegulatorRezimRada.Dnevni, dnevniK, dnevniP, 22, 19);
+            Console.WriteLine("Test123");
             Console.ReadLine();
         }
     }
