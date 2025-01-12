f7b07c757836ae3b78cc1aaecead1eae8041ce86
knezzevic <knezevicn154@gmail.com>
2025-01-12 19:14:57 +0100

    IHeaterService

---
+++ Services/HeaterServisi/HeaterService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Domain.Models;
+using Domain.Services;
+using Services.HeaterServisi;
+namespace Services.HeaterServisi
+{
+	public class HeaterService : IHeaterService
+	{
+        private readonly Heater _heater;
+
+        public HeaterService(Heater heater)
+        {
+            _heater = heater;
+        }
+
+        public void Ukljuci()
+        {
+            _heater.UkljuciPec();
+        }
+
+        public void Iskljuci()
+        {
+            _heater.IskljuciPec();
+        }
+
+    }
+}
+
